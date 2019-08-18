d057ae15c14aaf6b900e9f78d47cd1e426640b82
Daniel Buckley <1008669@student.sae.edu.au>
2019-08-19 09:41:12 +1000

    Added project.

---
+++ Assets/PlacementIndicator/PlacementIndicator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementIndicator : MonoBehaviour
+{
+    private ARRaycastManager rayManager;
+    private GameObject visual;
+
+    void Start ()
+    {
+        // get the components
+        rayManager = FindObjectOfType<ARRaycastManager>();
+        visual = transform.GetChild(0).gameObject;
+
+        // hide the placement indicator visual
+        visual.SetActive(false);
+    }
+    
+    void Update ()
+    {
+        // shoot a raycast from the center of the screen
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        // if we hit an AR plane surface, update the position and rotation
+        if(hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+
+            // enable the visual if it's disabled
+            if(!visual.activeInHierarchy)
+                visual.SetActive(true);
+        }
+    }
+}
